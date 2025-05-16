b21ed369a91647568037442b18dcd84ae4d74ff6
awakra <lucasadls0@gmail.com>
2025-05-16 17:55:44 -0300

    create game window and set deltatime

--- main.py
+++ main.py
@@ -4,6 +4,26 @@ import sys
 
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Breakout")
 clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    # Sets maximum framerate to 60 and gets delta time
+    dt = clock.tick(60) / 1000
+    for event in pygame.event.get():
+    
+
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill(BLACK)
+    # Cleans the screen using the black color
+    
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
+# Exit the program
